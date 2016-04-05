eceeedb2ab8c8012b0ab8e67d122b711710fd6ef
Jacob Sayles <jsayles@gmail.com>
2016-04-05 16:49:34 -0700

    added LandRecord model

--- C4D/admin.py
+++ C4D/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from models import Office
+from models import Office, LandRecord
 
 # Register your models here.
 admin.site.register(Office)
-
+admin.site.register(LandRecord)
--- C4D/models.py
+++ C4D/models.py
@@ -4,3 +4,6 @@ class Office(models.Model):
     name = models.CharField(max_length=200)
     airport_code = models.CharField(max_length=3)
 
+class LandRecord(models.Model):
+    grantor = models.CharField(max_length=64)
+    grantee = models.CharField(max_length=64)
