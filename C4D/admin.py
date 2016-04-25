1b8096f00961ea825ffac391fdc2273732f31ea3
Jacob Sayles <jsayles@gmail.com>
2016-04-25 13:22:45 -0700

    added more advanced models

--- C4D/admin.py
+++ C4D/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from models import Office, LandRecord
+from models import *
 
 # Register your models here.
 admin.site.register(Office)
 admin.site.register(LandRecord)
+admin.site.register(Property)
+admin.site.register(TitleCompany)
+admin.site.register(Municipality)
+admin.site.register(Island)
+admin.site.register(Condominium)
+admin.site.register(Transaction)
