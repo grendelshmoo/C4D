cbadbe1a6f2a7ead5262098cacb320a8c2784cfc
jacob <jsayles@gmail.com>
2016-05-27 15:29:34 -0700

    flattened RawLandRecord

--- C4D/management/commands/import.py
+++ C4D/management/commands/import.py
@@ -1,10 +1,12 @@
-from xlrd import open_workbook
+import xlrd
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
+from C4D.models import RawLandRecord
+
 def xls_to_dict(filename):
-    book = open_workbook(filename)
+    book = xlrd.open_workbook(filename)
     sheet = book.sheet_by_index(0)
     keys = [sheet.cell(0, col_index).value for col_index in xrange(sheet.ncols)]
     dict_list = []
@@ -28,3 +30,35 @@ class Command(BaseCommand):
         print("Loading '%s'..." % excel_file)
         rows = xls_to_dict(excel_file)
         print("Success!  Loaded %d rows." % len(rows))
+
+        for row in rows:
+            rlr = RawLandRecord()
+            rlr.office = row['office']
+            rlr.document_date = xlrd.xldate.xldate_as_datetime(row['document_date'], 0)
+            rlr.recording_date = xlrd.xldate.xldate_as_datetime(row['recording_date'], 0)
+            rlr.title_company = row['title_company']
+            rlr.legal_description = row['legal_description']
+            rlr.lot = row['lot']
+            rlr.block = row['block']
+            rlr.unit = row['unit']
+            rlr.area = row['area']
+            rlr.phase = row['phase']
+            rlr.tract = row['tract']
+            rlr.increment = row['increment']
+            rlr.lot_sf = row['lot_sf']
+            rlr.building_sf = row['building_sf']
+            rlr.map_document = row['map_document']
+            rlr.building_type = row['building_type']
+            rlr.year_built = row['year_built']
+            rlr.construction_type  = row['construction_type']
+            rlr.building_condition = row['building_condition']
+            rlr.island = row['island']
+            rlr.municipality = row['municipality']
+            rlr.condominium = row['condominium']
+            rlr.instrument_number = row['instrument_number']
+            rlr.fy_number = row['fy_number']
+            rlr.cnmi_file_numer = row['cnmi_file_numer']
+            rlr.lcdn = row['lcdn']
+            rlr.book = row['book']
+            rlr.page = row['page']
+            rlr.save()
--- C4D/models.py
+++ C4D/models.py
@@ -1,10 +1,44 @@
 from django.db import models
 
+
+class RawLandRecord(models.Model):
+    office = models.CharField(max_length=3)
+    document_date = models.DateField(blank=True, null=True)
+    recording_date = models.DateField(blank=True, null=True)
+    document_type = models.CharField(max_length=128, blank=True, null=True)
+    title_company = models.CharField(max_length=256, blank=True, null=True)
+    legal_description = models.CharField(max_length=256, blank=True, null=True)
+    lot = models.CharField(max_length=64, blank=True, null=True)
+    block = models.CharField(max_length=32, blank=True, null=True)
+    unit = models.CharField(max_length=32, blank=True, null=True)
+    area = models.FloatField(blank=True, null=True)
+    phase = models.CharField(max_length=32, blank=True, null=True)
+    tract = models.IntegerField()
+    increment = models.PositiveSmallIntegerField()
+    lot_sf = models.FloatField(blank=True, null=True)
+    building_sf = models.FloatField(blank=True, null=True)
+    map_document = models.IntegerField()
+    building_type = models.CharField(max_length=32, blank=True, null=True)
+    year_built = models.PositiveSmallIntegerField(blank=True, null=True)
+    construction_type  = models.CharField(max_length=32, blank=True, null=True)
+    building_condition = models.CharField(max_length=32, blank=True, null=True)
+    island = models.CharField(max_length=256, blank=True, null=True)
+    municipality = models.CharField(max_length=256, blank=True, null=True)
+    condominium = models.CharField(max_length=256, blank=True, null=True)
+    instrument_number = models.CharField(max_length=32, blank=True, null=True)
+    fy_number = models.CharField(max_length=64, blank=True, null=True)
+    cnmi_file_numer = models.CharField(max_length=32, blank=True, null=True)
+    lcdn = models.IntegerField(blank=True, null=True)
+    book = models.PositiveSmallIntegerField(blank=True, null=True)
+    page = models.PositiveSmallIntegerField(blank=True, null=True)
+
+
 class TitleCompany(models.Model):
     name = models.CharField(max_length=256)
 
     def __unicode__(self):
         return self.name
+
 
 class Municipality(models.Model):
     name = models.CharField(max_length=256)
@@ -12,11 +46,13 @@ class Municipality(models.Model):
     def __unicode__(self):
         return self.name
 
+
 class Condominium(models.Model):
     name = models.CharField(max_length=256)
 
     def __unicode__(self):
         return self.name
+
 
 # Change to choice field?
 class Island(models.Model):
@@ -25,12 +61,14 @@ class Island(models.Model):
     def __unicode__(self):
         return self.name
 
+
 class Office(models.Model):
     name = models.CharField(max_length=200)
     airport_code = models.CharField(max_length=3)
 
     def __unicode__(self):
         return "%s (%s) " % (self.name, self.airport_code)
+
 
 class Property(models.Model):
     legal_description = models.CharField(max_length=256, blank=True, null=True)
@@ -52,6 +90,7 @@ class Property(models.Model):
     municipality = models.ForeignKey(Municipality, blank=True, null=True)
     condominium = models.ForeignKey(Condominium, blank=True, null=True)
 
+
 class LandRecord(models.Model):
     office = models.ForeignKey(Office, null=False)
     property = models.ForeignKey(Property, null=False)
@@ -65,39 +104,7 @@ class LandRecord(models.Model):
     lcdn = models.IntegerField(blank=True, null=True)
     book = models.PositiveSmallIntegerField(blank=True, null=True)
     page = models.PositiveSmallIntegerField(blank=True, null=True)
-    #properties = models.ManyToManyField('Property', blank=True, null=True)
 
-class RawLandRecord(models.Model):
-    office = models.ForeignKey(Office, null=False)
-    document_date = models.DateField(blank=True, null=True)
-    recording_date = models.DateField(blank=True, null=True)
-    document_type = models.CharField(max_length=128, blank=True, null=True)
-    title_company = models.ForeignKey(TitleCompany, blank=True, null=True)
-    legal_description = models.CharField(max_length=256, blank=True, null=True)
-    lot = models.CharField(max_length=64, blank=True, null=True)
-    block = models.CharField(max_length=32, blank=True, null=True)
-    unit = models.CharField(max_length=32, blank=True, null=True)
-    area = models.FloatField(blank=True, null=True)
-    phase = models.CharField(max_length=32, blank=True, null=True)
-    tract = models.IntegerField()
-    increment = models.PositiveSmallIntegerField()
-    lot_sf = models.FloatField(blank=True, null=True)
-    building_sf = models.FloatField(blank=True, null=True)
-    map_document = models.IntegerField()
-    building_type = models.CharField(max_length=32, blank=True, null=True)
-    year_built = models.PositiveSmallIntegerField(blank=True, null=True)
-    construction_type  = models.CharField(max_length=32, blank=True, null=True)
-    building_condition = models.CharField(max_length=32, blank=True, null=True)
-    island = models.ForeignKey(Island, blank=False, null=False)
-    municipality = models.ForeignKey(Municipality, blank=True, null=True)
-    condominium = models.ForeignKey(Condominium, blank=True, null=True)
-    instrument_number = models.CharField(max_length=32, blank=True, null=True)
-    fy_number = models.CharField(max_length=64, blank=True, null=True)
-    cnmi_file_numer = models.CharField(max_length=32, blank=True, null=True)
-    lcdn = models.IntegerField(blank=True, null=True)
-    book = models.PositiveSmallIntegerField(blank=True, null=True)
-    page = models.PositiveSmallIntegerField(blank=True, null=True)
-    #properties = models.ManyToManyField('Property', blank=True, null=True)
 
 class Transaction(models.Model):
     # One to one?
