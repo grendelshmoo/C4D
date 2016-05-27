5277443fa7bdb72c93a731a15ce9ef9a7916b9da
jacob <jsayles@gmail.com>
2016-05-27 15:58:58 -0700

    hide other tables for now

--- C4D/admin.py
+++ C4D/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 
 from models import *
 
-admin.site.register(Office)
+#admin.site.register(Office)
 #admin.site.register(LandRecord)
 #admin.site.register(Property)
 admin.site.register(RawLandRecord)
-admin.site.register(TitleCompany)
-admin.site.register(Municipality)
-admin.site.register(Island)
-admin.site.register(Condominium)
-admin.site.register(Transaction)
+#admin.site.register(TitleCompany)
+#admin.site.register(Municipality)
+#admin.site.register(Island)
+#admin.site.register(Condominium)
+#admin.site.register(Transaction)
