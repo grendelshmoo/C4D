f98e682a033f0f651547865c60712f1cdc2d5a85
jacob <jsayles@gmail.com>
2016-05-29 10:16:06 -0700

    added search fields and filters

--- C4D/admin.py
+++ C4D/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 
 from models import *
 
+class RawLandRecordAdmin(admin.ModelAdmin):
+    search_fields = ('legal_description', )
+    list_display=('id', 'office', 'island', 'document_date', 'legal_description')
+    list_filter=('office', 'island')
+admin.site.register(RawLandRecord, RawLandRecordAdmin)
+
+# Normalized tables for later version
 #admin.site.register(Office)
 #admin.site.register(LandRecord)
 #admin.site.register(Property)
-admin.site.register(RawLandRecord)
 #admin.site.register(TitleCompany)
 #admin.site.register(Municipality)
 #admin.site.register(Island)
