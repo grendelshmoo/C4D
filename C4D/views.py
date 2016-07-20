25b29b402c10fd003bffd0e221a5ba4c66684689
jacob <jsayles@gmail.com>
2016-07-20 15:19:20 -0700

    now make sure you can add record before you can import

--- C4D/views.py
+++ C4D/views.py
@@ -3,9 +3,8 @@ from django.template import RequestContext, Template
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden, Http404
 from django.shortcuts import render, render_to_response, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
 
-@login_required
 def home(request):
     return render_to_response('home.html',{'has_permission':True}, RequestContext(request))
 
@@ -14,5 +13,6 @@ def search(request):
     return render_to_response('search.html',{'has_permission':True}, RequestContext(request))
 
 @login_required
+@permission_required('C4D.add_rawlandrecord', login_url='/')
 def import_file(request):
     return render_to_response('import_file.html',{'has_permission':True}, RequestContext(request))
