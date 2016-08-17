4646dabcb6a6b37f5cde3383ac48b0f6005498a9
jacob <jsayles@gmail.com>
2016-08-17 09:21:06 -0700

    fixed bug in restore

--- C4D/management/commands/restore_backup.py
+++ C4D/management/commands/restore_backup.py
@@ -6,7 +6,8 @@ import sys
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-from staff.backup import BackupManager
+
+from C4D.backup import BackupManager
 
 
 class Command(BaseCommand):
